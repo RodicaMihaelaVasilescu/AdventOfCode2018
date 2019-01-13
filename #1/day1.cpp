ebca5501a572e8b6dda21fe074d9c08ef51e08fb
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-01-13 20:07:34 +0200

    Day1

---
+++ #1/day1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Part2(vector<int> input)
+{
+    map<int, int> frequency;
+    int sum = 0, index = -1;
+    while(true)
+    {
+        ++index %= input.size();
+        sum += input[index];
+        frequency[sum]++;
+
+        if(frequency[sum] == 2)
+        {
+            return sum;
+        }
+    }
+}
+int main ()
+{
+    ifstream cin("in.txt");
+    ofstream cout("out.txt");
+
+    vector <int> input;
+    string inputLine;
+    while(getline (cin, inputLine))
+    {
+        input.push_back(stoi(inputLine));
+    }
+
+    cout << "Part1: " << accumulate(input.begin(), input.end(), 0) << endl;
+    cout << "Part2: " << Part2(input) << endl;
+
+    return 0;
+}
