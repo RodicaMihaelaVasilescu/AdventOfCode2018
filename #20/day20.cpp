ceb828e56f22ce215c267bd510afb3fa29751f96
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2019-02-09 21:37:32 +0200

    Day20

--- #20/day20.cpp
+++ #20/day20.cpp
@@ -1,21 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
-#include <vector>
+#include <set>
 #include <map>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
-
-#define DOOR 'D'
-#define ROOM 'R'
 
 map<char, int> directionsToInteger = { {'N', 0}, {'S', 1}, {'E', 2}, {'W', 3} };
 int row[] = { -1, 1, 0, 0 };
 int col[] = { 0, 0, 1,-1 };
 
-map<pair<int, int>, char> CoordonatesOfRoomsAndDoors;
-int index;
+int index, MaximumNumberOfDoors;
+map<pair<int, int>, int> CoordonatesOfRooms;
+set<pair<int, int>> CoordinatesOfRoomsWithAtLeast1000Doors;
 
 void generateMap(string regex, pair<int, int> coordinates = make_pair(0, 0))
 {
@@ -26,10 +24,15 @@ void generateMap(string regex, pair<int, int> coordinates = make_pair(0, 0))
 		{
 			int x = currentCoordinates.first + row[directionsToInteger[regex[index]]];
 			int y = currentCoordinates.second + col[directionsToInteger[regex[index]]];
-			CoordonatesOfRoomsAndDoors[make_pair(x, y)] = DOOR;
-			x += row[directionsToInteger[regex[index]]];
-			y += col[directionsToInteger[regex[index]]];
-			CoordonatesOfRoomsAndDoors[make_pair(x, y)] = ROOM;
+			if (CoordonatesOfRooms[make_pair(x, y)] == 0)
+			{
+				CoordonatesOfRooms[make_pair(x, y)] = CoordonatesOfRooms[currentCoordinates] + 1;
+			}
+			MaximumNumberOfDoors = max(CoordonatesOfRooms[make_pair(x, y)], MaximumNumberOfDoors);
+			if (CoordonatesOfRooms[make_pair(x, y)] >= 1000)
+			{
+				CoordinatesOfRoomsWithAtLeast1000Doors.insert(make_pair(x, y));
+			}
 			currentCoordinates = make_pair(x, y);
 		}
 		else if (regex[index] == '(')
@@ -47,39 +50,6 @@ void generateMap(string regex, pair<int, int> coordinates = make_pair(0, 0))
 	}
 }
 
-void Solve()
-{
-	int MaximumNumberOfRooms = 0, AtLeast1000Doors = 0;
-	map<pair<int, int>, int> CoordinatesOfRooms_NumberOfDoors;
-	queue<pair<int, int>> CoordinatesOfRoomsQueue;
-	CoordinatesOfRoomsQueue.push(make_pair(0, 0));
-	while (!CoordinatesOfRoomsQueue.empty())
-	{
-		auto coordinates = CoordinatesOfRoomsQueue.front();
-		CoordinatesOfRoomsQueue.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int x = coordinates.first + row[i];
-			int y = coordinates.second + col[i];
-			if (CoordonatesOfRoomsAndDoors[make_pair(x, y)] == DOOR)
-			{
-				x += row[i];
-				y += col[i];
-				if (CoordinatesOfRooms_NumberOfDoors[make_pair(x, y)] == 0)
-				{
-					CoordinatesOfRooms_NumberOfDoors[make_pair(x, y)] = CoordinatesOfRooms_NumberOfDoors[coordinates] + 1;
-					CoordinatesOfRoomsQueue.push(make_pair(x, y));
-
-					MaximumNumberOfRooms = max(CoordinatesOfRooms_NumberOfDoors[make_pair(x, y)], MaximumNumberOfRooms);
-					CoordinatesOfRooms_NumberOfDoors[make_pair(x, y)] >= 1000 ? AtLeast1000Doors++ : 0;
-				}
-			}
-		}
-	}
-	cout << "Part1: " << MaximumNumberOfRooms << endl;
-	cout << "Part2: " << AtLeast1000Doors << endl;
-}
 int main()
 {
 	freopen("input.txt", "r", stdin);
@@ -88,6 +58,7 @@ int main()
 	string input;
 	cin >> input;
 	generateMap(input);
-	Solve();
+	cout << "Part1: " << MaximumNumberOfDoors << endl;
+	cout << "Part2: " << CoordinatesOfRoomsWithAtLeast1000Doors.size() << endl;
 	return 0;
 }
